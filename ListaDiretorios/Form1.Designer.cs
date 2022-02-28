97cbc49d3b3740b9ee552c8a7ee103ee8fd4e285
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-27 22:09:26 -0300

    220928022022F

---
+++ ListaDiretorios/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace ListaDiretorios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.list_Directorios = new System.Windows.Forms.ListBox();
+            this.btn_Listar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista de pastas e arquivos";
+            // 
+            // list_Directorios
+            // 
+            this.list_Directorios.FormattingEnabled = true;
+            this.list_Directorios.ItemHeight = 20;
+            this.list_Directorios.Location = new System.Drawing.Point(12, 81);
+            this.list_Directorios.Name = "list_Directorios";
+            this.list_Directorios.Size = new System.Drawing.Size(691, 304);
+            this.list_Directorios.TabIndex = 1;
+            // 
+            // btn_Listar
+            // 
+            this.btn_Listar.Location = new System.Drawing.Point(269, 404);
+            this.btn_Listar.Name = "btn_Listar";
+            this.btn_Listar.Size = new System.Drawing.Size(177, 59);
+            this.btn_Listar.TabIndex = 2;
+            this.btn_Listar.Text = "Listar";
+            this.btn_Listar.UseVisualStyleBackColor = true;
+            this.btn_Listar.Click += new System.EventHandler(this.btn_Listar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 489);
+            this.Controls.Add(this.btn_Listar);
+            this.Controls.Add(this.list_Directorios);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista Diretorios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox list_Directorios;
+        private System.Windows.Forms.Button btn_Listar;
+    }
+}
+
---
+++ ListaDiretorios/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaDiretorios
+{
+    public partial class Form1 : Form
+    {
+        //@"d:\dados"
+        string path = @"d:\dados";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Listar_Click(object sender, EventArgs e)
+        {
+            using(var openDlg = new FolderBrowserDialog())
+            {
+                openDlg.SelectedPath = path;
+                if(openDlg.ShowDialog() == DialogResult.OK)
+                {
+                    path = openDlg.SelectedPath; // Caminho da pasta selecionada;
+                }
+            }
+
+            list_Directorios.Items.Clear();
+            GetDir(path);
+            GetFiles(path);
+        }
+
+        private void GetFiles(string files)
+        {
+            var infiles = Directory.GetFiles(files);
+            foreach(string file in infiles)
+            {
+                list_Directorios.Items.Add(file + "<Arquivo>");
+            }
+        }
+
+        private void GetDir(string path)
+        {
+            var diretorios = Directory.GetDirectories(path);
+            foreach(string dir in diretorios)
+            {
+                list_Directorios.Items.Add(dir +"<Directory>");
+            }
+        }
+    }
+}
